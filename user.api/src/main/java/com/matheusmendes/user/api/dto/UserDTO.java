49725c3d2cc9c84b070ccdcaceb3cb8d0dd0b997
matheus <matheusmendesreis@outlook.com>
2025-03-31 15:02:33 -0300

    feat: add new atribut in class

--- user.api/src/main/java/com/matheusmendes/user/api/dto/UserDTO.java
+++ user.api/src/main/java/com/matheusmendes/user/api/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.matheusmendes.user.api.dto;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.matheusmendes.user.api.model.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,9 +16,23 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserDTO {
     
+    private UUID id;
     private String nome;
     private String cpf;
     private String telefone;
     private String endereco;
     private LocalDateTime dataCadastro;
+
+
+    public static UserDTO convert(User usr){
+        UserDTO user = new UserDTO();
+        user.setId(usr.getId());
+        user.setNome(usr.getNome());
+        user.setCpf(usr.getCpf());
+        user.setTelefone(usr.getTelefone());
+        user.setEndereco(usr.getEndereco());
+        user.setDataCadastro(usr.getDataCadastro());
+        
+        return user;
+    };
 }
