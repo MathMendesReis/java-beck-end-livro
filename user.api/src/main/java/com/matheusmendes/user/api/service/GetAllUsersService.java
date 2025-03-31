17d08b4bc358e2abba070b5d29008966e76cbaf2
matheus <matheusmendesreis@outlook.com>
2025-03-31 15:04:07 -0300

    feat:create new service, get all users

---
+++ user.api/src/main/java/com/matheusmendes/user/api/service/GetAllUsersService.java
@@ -0,0 +1,26 @@
+package com.matheusmendes.user.api.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.matheusmendes.user.api.dto.UserDTO;
+import com.matheusmendes.user.api.model.User;
+import com.matheusmendes.user.api.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GetAllUsersService {
+    private final UserRepository userRepository;
+
+    public List<UserDTO> execute(){
+        List<User> users = userRepository.findAll();
+        return users
+        .stream()
+        .map(UserDTO::convert)
+        .collect(Collectors.toList());
+    }
+}
